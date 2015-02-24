4ef774bef78fd95f62ec653929609853272b0294
Nagato KASAKI <kasaki-nagato@dmm.com>
2015-02-24 15:46:05 +0900

    add update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+gunzip GeoIP.dat.gz
+mv GeoIP.dat priv/GeoIP.dat
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+gunzip GeoLiteCity.dat.gz
+mv GeoLiteCity.dat priv/GeoLiteCity.dat
+
